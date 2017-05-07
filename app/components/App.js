978000b05766705f607132ee8dcb6a333b41cd98
Charlie Byers <charlie@Charlies-MacBook-Air.local>
2017-05-07 14:02:48 +0100

    First React App commit

---
+++ app/components/App.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Hello = React.createClass({
+  render: function() {
+    return <div>
+        <h1>Hello.</h1>
+        <h2>Is it me you're looking for?</h2>
+      </div>;
+  }
+});
+
+module.exports = Hello
